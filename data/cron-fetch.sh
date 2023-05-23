51be33e2d829e04cf952e91b9bbfe9aaa89dc564
Alex Euler <0xalexeuler@gmail.com>
2023-05-23 12:17:07 +0400

    fixes

--- data/cron-fetch.sh
+++ data/cron-fetch.sh
@@ -1,22 +1,11 @@
 echo "Source .env..."
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/.env
-echo "Just run poetry..."
-UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry
-echo $?
-echo "Just run python..."
-UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry run python -V
-echo $?
-echo "Just run q python..."
-UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry run -q python
-echo $?
-
-echo "Just run main..."
-UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry run -q python main.py
-echo $?
+pushd $SCRIPT_DIR
 echo "Fetching data..."
-UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry run -q python main.py fetch-pools >> /root/uniearn/logs/uniearn_cron-fetch.log 2>&1
+UNIEARN_DATA_LOG_LEVEL=DEBUG /usr/local/bin/poetry run -q python main.py fetch-pools
 echo $?
 echo "Processing data..."
-/usr/local/bin/poetry run -q python main.py process-pools 2>&1
+/usr/local/bin/poetry run -q python main.py process-pools
 echo $?
+popd
