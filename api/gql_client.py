af34710c401670388d0e3390407e85f0744724fd
Alex Euler <0xalexeuler@gmail.com>
2023-05-28 13:34:00 +0400

    fix

--- api/gql_client.py
+++ api/gql_client.py
@@ -1,9 +1,76 @@
 from functools import cache
+import backoff
+import asyncio
 from gql import Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
 # from gql.transport.requests import RequestsHTTPTransport
 from base import Base
+
+
+class GraphQLEngineClient(Base):
+    _url: str
+
+    def __init__(self, url, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._url = url
+        self._client = Client(
+            transport=AIOHTTPTransport(url=url),
+            fetch_schema_from_transport=True,
+        )
+        self._session = None
+        self._connect_task = None
+        self._close_event = asyncio.Event()
+
+    @backoff.on_exception(backoff.expo, Exception, max_time=300)
+    async def _connection(self):
+        self.logger.debug(f"Connecting to {self._url}")
+
+        try:
+            async with self._client as session:
+                self._session = session
+                self.logger.debug(f"Connected to {self._url}")
+
+                # Wait for the close event
+                self._close_event.clear()
+                await self._close_event.wait()
+
+                self._session = None
+                self._connect_task = None
+
+                self.logger.debug(f"Closed {self._url}")
+                return
+
+        finally:
+            self._session = None
+            self._connect_task = None
+            self.logger.debug(f"Disconnected {self._url}")
+
+    def connect(self):
+        if not self._connect_task:
+            self._connect_task = asyncio.create_task(self._connection())
+
+    def close(self):
+        self._close_event.set()
+
+    async def execute(self, document):
+        retries = 3
+        while True:
+            if not self._session:
+                self.connect()
+                retries -= 1
+                if retries == 0:
+                    raise Exception("Not connected")
+                await asyncio.sleep(1)
+                continue
+
+            try:
+                return await self._session.execute(document)
+            except Exception as e:
+                self.logger.debug(f"Error: {e}, retries left {reties}")
+                reties -= 1
+                if reties == 0:
+                    raise e
 
 
 class GQLClient(Base):
@@ -14,18 +81,8 @@ class GQLClient(Base):
         self._uniswap_config = uniswap_config
 
     async def request(self, query: str, chain_id: int):
-        client = self.client(chain_id)
-        reties = 3
-        while True:
-            try:
-                return await client.execute_async(query)
-            except Exception as e:
-                self.logger.debug(f"Error: {e}, retries left {reties}")
-                reties -= 1
-                if reties == 0:
-                    raise e
+        return await self.client(chain_id).execute(query)
 
     @cache
-    def client(self, chain_id: int):
-        transport = AIOHTTPTransport(url=self._uniswap_config["graphql"][chain_id])
-        return Client(transport=transport, fetch_schema_from_transport=True)
+    def client(self, chain_id: int) -> GraphQLEngineClient:
+        return GraphQLEngineClient(self._uniswap_config["graphql"][chain_id])
